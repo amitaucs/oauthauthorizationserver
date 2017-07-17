8d498d409049860c92969134294d0f68b98f2418
amitdatta <the.amistudy@gmail.com>
2017-07-17 12:37:13 +0530

    oauth defect fixing

--- src/main/java/com/amisoft/AmisoftApplication.java
+++ src/main/java/com/amisoft/AmisoftApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import java.security.Principal;
 @SpringBootApplication
 @EnableAuthorizationServer
 @EnableDiscoveryClient
+@EnableResourceServer
 @RestController
 public class AmisoftApplication {
 
